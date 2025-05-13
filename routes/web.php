a0a4480a26d75e5c43d6357e5d78c7b1ef06bb3a
altughf <altughf@gmail.com>
2025-05-13 17:49:29 +0300

    Category Route
    Add routes.

--- routes/web.php
+++ routes/web.php
@@ -34,4 +34,12 @@ Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.upda
 Route::patch('/tasks/{task}', [TaskController::class, 'status'])->name('tasks.status');
 Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
 
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
+Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+
 require __DIR__.'/auth.php';
