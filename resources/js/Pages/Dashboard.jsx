8c7a6c9932764fa444992d18512634752c82164e
altughf <altughf@gmail.com>
2025-05-17 12:44:21 +0300

    Dashboard
    Simple dashboard for tasks, categories, completed tasks & due soon.

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -7,13 +7,39 @@ export default function Dashboard() {
         <>
             <Head title="Dashboard" />
 
-            <div className="py-12">
+            <div className="py-4">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            You're logged in!
+
+                    <div className='flex w-full h-[640px] gap-4'>
+
+                        <div className='flex flex-col w-full h-full gap-4 bg-neutral-400'>
+
+                            <div className='flex w-full h-full gap-4'>
+                                <div className='flex w-full h-full bg-neutral-500'>
+                                    <div>TASKS</div>
+
+                                </div>
+                                <div className='flex w-full h-full bg-neutral-500'>
+                                    <div>CATEGORIES</div>
+
+                                </div>
+                            </div>
+
+                            <div className='flex w-full h-full bg-neutral-500'>
+                                <div>COMPLETED TASKS</div>
+
+                            </div>
+
                         </div>
+
+                        <div className='flex w-full h-full max-w-lg bg-neutral-400'>
+
+                            <div>DUE SOON</div>
+
+                        </div>
+
                     </div>
+
                 </div>
             </div>
         </>
